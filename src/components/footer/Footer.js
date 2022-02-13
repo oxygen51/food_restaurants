a42c6ade1d5f74ba9bcc6efe98c208e06c38c756
oxygen51 <kallolmohammad@gmail.com>
2022-02-13 14:06:23 +0600

    Update footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,9 +2,19 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div>
-
-        </div>
+        <div class="">
+            <footer class="footer" style={{ marginTop: "30px" }}>
+                <div class="container-flix bg-danger" style={{ height: "100px" }}>
+                    <span class="text-dark font-italic h1" style={{ float: "right", marginRight: "20px", marginTop: "20px" }}>This is footer</span>
+                    <ul style={{ float: "left" }} >
+                        <li style={{ listStyleType: "none" }}><a href="https://google.com/" target="_blank" style={{ textDecoration: "none" }}>nOOb Resturent</a></li>
+                        <li style={{ listStyleType: "none" }}><a href="https://google.com/" target="_blank" style={{ textDecoration: "none" }}>nOOb Resturent</a></li>
+                        <li style={{ listStyleType: "none" }}><a href="https://google.com/" target="_blank" style={{ textDecoration: "none" }}>nOOb Resturent</a></li>
+                        <li style={{ listStyleType: "none" }}><a href="https://google.com/" target="_blank" style={{ textDecoration: "none" }}>nOOb Resturent</a></li>
+                    </ul>
+                </div>
+            </footer >
+        </div >
     );
 }
 export default Footer;
